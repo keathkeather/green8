f87851823ea8edee3c09c2125b9b955d83ac504f
shaniacc <canoy.shania@gmail.com>
2024-03-02 00:23:40 +0800

    fixed font bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+          />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );

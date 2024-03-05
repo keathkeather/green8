d0f3f83315c9b60a294f37c92496b760d727194b
keathkeather <keath.ian@gmail.com>
2024-03-06 03:26:10 +0800

    [KET][UPDATE] bug fix on contact us & AnimatedText components

--- app/components/AnimatedText.tsx
+++ app/components/AnimatedText.tsx
@@ -7,7 +7,7 @@ export default function AnimatedText() {
         <div className="animate ">
           {
             [0,1,2,3,4,5,6,7,8,9,10,11].map((i) => (
-            <div className="text-[#E5e5e5] font-bold text-[24px] tracking-[10px] font-montserrat bold whitespace-nowrap inline-flex items-center justify-center">
+            <div key={i} className="text-[#E5e5e5] font-bold text-[24px] tracking-[10px] font-montserrat bold whitespace-nowrap inline-flex items-center justify-center">
                 FOCUS ON YOUR 
                 <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#FFC978] to-[#6FCF97] '>&nbsp;FITNESS&nbsp;</span>
                 NOT YOUR LOSS •&nbsp;
